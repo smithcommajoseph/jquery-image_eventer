050fc40ebec7b59648ae5bf1715a6d9c65c073ec
Joseph Smith <technicolorenvy@gmail.com>
2011-09-29 23:23:40 -0600

    making a simple version work with an array of imgs

--- jquery.pil.js
+++ jquery.pil.js
@@ -8,18 +8,57 @@
 
 (function($){
 	
-	var ver = '1.0';
+	var ver = '0.1',
+		$this,
+		$body = $('body'),
+		$tmpCont = $('<div id="pil-tmp-container" style="display: none !important"/>'),
+		queue,
+		i = 0;
 	
-	var $pil = $.pil = function(arg1, arg2){
-		$(window).pil(arg1, arg2);
-	};
+	function _setQueue(a, b){
+		var queue = [],
+			e;
+		
+		switch(typeof a){ 
+			case 'object':
+				if(_isArray(a)){
+					e = (b !== undefined && typeof b == 'string') ? b : 'pil::imagesLoadComplete';
+					queue.push({files: a, completedEvent: e});
+				}else{
+					//we have a map, do map stuff...
+				}
+				break;
+			default:
+				//if your using a modern browser, you'll get a decent error.
+				console.error('You have provided the wrong kind of argument.');
+				console.warn('Please pass an Array or Object as your first arg to $.pil');
+				break;
+		}
+		
+		return queue;
+	}
 	
-	/****************************************************
-	 * _allTrues:boolean
-	 * 
-	 * checks the values for each prop are true
-	 ****************************************************
-	/                                                  */
+	function _eventController(queue){
+		var j,
+			qL = queue.length,
+			fL;
+			
+		$this.bind('pil::tmpLoadComplete', _tmpLoadComplete);
+		$body.append($tmpCont);
+			
+		fL = queue[i].files.length-1;
+		for(j=fL; j>=0; j--){
+			$tmpCont.append('<img src="'+queue[i].files[j]+'" alt="stubbed" />');
+		}
+		// console.log(queue[i].completedEvent);
+		_checkImageLoad($tmpCont.find('img'));
+
+	}
+	
+	function _isArray(a){
+		return a.constructor == (new Array).constructor;
+	}
+	
 	function _allTrues(obj){
 		var key,
 			alltrue = [];
@@ -34,13 +73,18 @@
 	
 	}
 	
-	/****************************************************
-	 * _imageFirstLoads:void
-	 * 
-	 * Check to see if we have loaded all the images
-	 ****************************************************
-	/                                                  */
-	function _imageFirstLoads($collection, event){
+	function _tmpLoadComplete(e){
+		//trigger EXTERNAL event here
+		$this.trigger(queue[i].completedEvent);
+		$this.unbind('pil::tmpLoadComplete', _tmpLoadComplete);
+		$tmpCont.remove();
+		if(typeof queue[i+1] !== 'undefined'){
+			i++;
+			_eventController(queue);
+		}
+	}
+	
+	function _checkImageLoad($collection){
 		var isLoaded = {};
 		
 		$.each($collection, function(){
@@ -53,7 +97,7 @@
 				
 				if(_allTrues(isLoaded)){
 					$collection.unbind('load');
-					//trigger event here
+					$this.trigger('pil::tmpLoadComplete');
 				}
 			 })
 			.each(function(index) { // check complete in case background images load from cache
@@ -63,18 +107,15 @@
             });
 	}
 	
-	$.fn.pil = function(arg1, arg2){
-		var o = { s: this.selector, c: this.context };
-
-		return this.each(function(){
+	$.fn.pil = function(a, b){
+		$this = $(this);
+		queue = _setQueue(a, b);
 			
+		return this.each(function(){
+			_eventController(queue);
 		});
 	};
 	
 	$.fn.pil.ver = function() { return ver; };
-	
-	$.fn.pil.defaults = {
-		
-	};
 	
 })(jQuery);
